7757aa274b25bb26ef524e6a8d2260742f365c6c
Emoncr <biplobemon 75945@gmail.com>
2024-01-22 23:57:51 +0600

    created jwt token genarator and verify token utils

--- src/utils/JWTHelper.js
+++ src/utils/JWTHelper.js
@@ -1,1 +1,22 @@
-export const GenarateToken = () => {};
+import { SignJWT, jwtVerify } from "jose";
+
+export const GenarateToken = async (email, id) => {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
+
+  const payload = { email, id };
+  const token = await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setExpirationTime(process.env.JWT_EXPIRES_IN)
+    .setIssuedAt()
+    .setSubject(payload)
+    .sign(secret);
+
+  return token;
+};
+
+export const VerifyToken = async (token) => {
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
+  const decodedToken = jwtVerify(token, secret);
+
+  return decodedToken["payload"];
+};
