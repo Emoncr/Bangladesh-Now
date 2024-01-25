f163854ce59f0239dde743deb521de001c2bc169
Emoncr <biplobemon 75945@gmail.com>
2024-01-26 00:54:13 +0600

    profile details api created

--- src/app/api/user/profile/route.js
+++ src/app/api/user/profile/route.js
@@ -1,15 +1,25 @@
+import { throwError } from "@/utils/response";
 import { PrismaClient } from "@prisma/client";
-import { useParams } from "next/navigation";
 import { NextResponse } from "next/server";
 
 export const GET = async (req, res, next) => {
   try {
     const { searchParams } = new URL(req.url);
-    console.log(searchParams);
-    const id = searchParams.get("id");
-    const prisma = new PrismaClient();
+    const paramsId =parseInt(searchParams.get("id"));
+    const requestId = parseInt(req.headers.get("id"));
+    if (requestId !== paramsId) return throwError(401, "User isn't valid");
 
+    const prisma = new PrismaClient(); 
 
+    const user = await prisma.Users.findUnique({
+      where: { id: requestId },
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "User founded",
+      user,
+    });
   } catch (error) {
     return NextResponse.json(
       { success: false, message: "User not found" },
