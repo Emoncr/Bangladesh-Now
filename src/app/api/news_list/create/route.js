b441606dae32ef7484494af2502937ef9b787139
Emoncr <biplobemon 75945@gmail.com>
2024-01-28 01:34:29 +0600

    news create api completed

---
+++ src/app/api/news_list/create/route.js
@@ -0,0 +1,27 @@
+import { throwError } from "@/utils/response";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export const POST = async (req, res, next) => {
+  try {
+    const reqBody = await req.json();
+    const prisma = new PrismaClient();
+
+
+
+    const news = await prisma.News_list.create({
+      data: reqBody,
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "news created successfully",
+        news,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json({ success: false, error:error.toString() }, { status: 400 });
+  }
+};
